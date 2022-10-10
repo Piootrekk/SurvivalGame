e8886d8baaf8d2b3011957fd2df21e5862582e14
Piootrekk <piotr.wilkowiecki100@gmail.com>
2022-10-10 23:41:48 +0200

    Colliders / Hitboxes

---
+++ Survival Game/Assets/Scripts/HitBoxManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBoxManager : MonoBehaviour
+{
+    [SerializeField] List<Transform> hitBoxes = new List<Transform>();
+}
